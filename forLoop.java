92a7a3a11d67ddad0aabe021744bdecd9636e0a1
Saurabh Manral <saurabhmanral08@gmail.com>
2020-04-15 15:37:36 -0700

    second commit

--- forLoop.java
+++ forLoop.java
@@ -2,6 +2,7 @@ public class forLoop {
 	public static void main(String args[]) {
 		for (int i = 0; i < 5; i++) {
 			System.out.println("This is for loop");
+			System.out.println("this is second statement");
 		}
 	}
 }
